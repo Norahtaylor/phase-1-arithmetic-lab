c089703cacd91c76cab3f6491c65c926326b616c
[Norah Taylor] <emt368@gmail.com>
2022-12-02 14:41:01 -0500

    commit tree d0bf4d8626de266e53e7cee0f7b823e5c209d112

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// const multiply = num1 * num2
+// multiply(2,31)
 
-// Write your code here
+const multiply = 2 * 31
+
+const random = Math.floor(Math.random()* 2) + 1
+
+const mod = 4 ** num3 
+
+const max = Math.max(1,3,10)
